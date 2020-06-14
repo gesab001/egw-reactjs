4f05226aff7b6d9891d212f9ed589abf0d2521ee
gesab001 <144000giovanni@gmail.com>
2020-06-15 11:16:51 +1200

    Updates

---
+++ precache-manifest.cbc3ae14cd85073d5e361f54524e66fa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "06bf6b83eb7e152c9fa7cbc53f8556d1",
+    "url": "/egw-reactjs/index.html"
+  },
+  {
+    "revision": "2a581b5e11df0a9fad7c",
+    "url": "/egw-reactjs/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "0f740615f53dc7fd2b52",
+    "url": "/egw-reactjs/static/js/2.ca35798e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/egw-reactjs/static/js/2.ca35798e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2a581b5e11df0a9fad7c",
+    "url": "/egw-reactjs/static/js/main.26862acb.chunk.js"
+  },
+  {
+    "revision": "90274713c7a42b34b3ce",
+    "url": "/egw-reactjs/static/js/runtime-main.446343c6.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/egw-reactjs/static/media/logo.5d5d9eef.svg"
+  }
+]);
