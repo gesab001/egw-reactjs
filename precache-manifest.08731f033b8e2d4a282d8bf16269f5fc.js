c5dd1af62960614cedd1d32cdfc762c65550cf60
gesab001 <144000giovanni@gmail.com>
2020-06-20 19:33:23 +1200

    Updates

--- precache-manifest.08731f033b8e2d4a282d8bf16269f5fc.js
+++ precache-manifest.08731f033b8e2d4a282d8bf16269f5fc.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "3de1744db36ecdcb378e9700a800630d",
+    "revision": "7f4eb0f4ed03ad17fd405d75832c5874",
     "url": "/egw-reactjs/index.html"
   },
   {
-    "revision": "bface05a610830ef05a1",
-    "url": "/egw-reactjs/static/css/main.9a114d8b.chunk.css"
+    "revision": "134f517415290502bfde",
+    "url": "/egw-reactjs/static/css/main.b92b1944.chunk.css"
   },
   {
     "revision": "08ad73138cb941b8a7a4",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/egw-reactjs/static/js/2.c731f9a4.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "bface05a610830ef05a1",
-    "url": "/egw-reactjs/static/js/main.2a070f9e.chunk.js"
+    "revision": "134f517415290502bfde",
+    "url": "/egw-reactjs/static/js/main.9bdbd7ea.chunk.js"
   },
   {
     "revision": "90274713c7a42b34b3ce",
