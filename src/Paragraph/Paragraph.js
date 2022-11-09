2c98facf2ba5398b35ae8c6a8ed01b72150b2477
giovanni <144000giovanni@gmail.com>
2022-11-10 03:37:05 +1300

    'update'

--- src/Paragraph/Paragraph.js
+++ src/Paragraph/Paragraph.js
@@ -2,6 +2,59 @@ import React from 'react';
 import TextToSpeech from '../TextToSpeech/TextToSpeech';
 import './paragraph.css';
 
+function    checkIfItsABookBible(_string){
+	  var bookNames = [
+
+		'1 Samuel',
+		'2 Samuel',
+		'1 Kings',
+		'2 Kings',
+		'1 Chronicles',
+		'2 Chronicles',
+		'1 Corinthians',
+		'2 Corinthians',
+		'1 Thessalonians',
+		'2 Thessalonians',
+		'1 Timothy',
+		'2 Timothy',
+		'1 Peter',
+		'2 Peter',
+		'1 John',
+		'2 John',
+		'3 John'
+	  ];
+	  var result;
+	  if (new RegExp(bookNames.join("|")).test(_string)) {
+		 //alert(_string + "is a book");
+		 result = true;
+	  }else{
+		 result = false;
+	  }
+	  return result;
+	}
+	
+function getWordwithoutNumbers(word){
+	    word = word.replace(/\n/g, " ");
+        let pattern =  /\d+\s\w+/g;
+		let result = word.match(pattern);
+		console.log(word);
+		console.log("result: " + result);
+		try{
+			for (var x=0; x<result.length;x++){
+			   var item = result[x];
+			   if (checkIfItsABookBible(item)){
+			   }else{
+				 let pattern = /\d+/g;
+				 var pagenumber = item.match(pattern); 
+				 word = word.replace(pagenumber, " " );
+
+			   };
+			}
+		}catch(e){
+		  //alert(word + e);
+		}
+		return word;
+}
 class Paragraph extends React.Component {
   constructor(props) {
     super(props);
@@ -22,7 +75,7 @@ class Paragraph extends React.Component {
           this.setState({
             isLoaded: true,
             items: result,
-            word: result.word//this.removeNumbers(result.word)
+            word: this.removeNumbers(result.word)
           });
         },
         // Note: it's important to handle errors here
@@ -37,11 +90,10 @@ class Paragraph extends React.Component {
       )
   }
 
-  removeNumbers(word){
-     console.log("remove numbers"); 
-     const withoutNumbers = word.replace(/\s+\d+\s+/g, ' ')
-     return withoutNumbers;
-  }
+ 
+   removeNumbers(word){
+      return getWordwithoutNumbers(word);
+   }
   
   render() {
     const { error, isLoaded, items } = this.state;
